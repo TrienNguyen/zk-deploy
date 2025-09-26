a56a35690e27186d1b517415ff3014e501009bd4
Kyo Loan <kyo.loan@freetrend.com.vn>
2025-09-26 14:36:52 +0700

    commit

--- FunnyProject/src/main/Unit16.java
+++ FunnyProject/src/main/Unit16.java
@@ -26,28 +26,23 @@ public class Unit16 extends SelectorComposer<Component> {
 	Window windowMaster;
 
 	@Wire
-	Textbox txtSoHang11, txtSoHang0, txtSoHang1, txtSoHang2, txtSoHang3, 
-	txtSoHang4, txtSoHang5, txtSoHang6, txtSoHang7, txtSoHang8, txtSoHang9, txtSoHang10 ;
+	Textbox txtSoHang11, txtSoHang0, txtSoHang1, txtSoHang2, txtSoHang3, txtSoHang4, txtSoHang5, txtSoHang6, txtSoHang7,
+			txtSoHang8, txtSoHang9, txtSoHang10;
 
 	@Wire
 	Button btnMain, btnReset, btnCheck;
-
-	
 
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		// TODO Auto-generated method stub
 		super.doAfterCompose(comp);
 		ArrayPattern();
-		
 	}
 
 	@Listen("onClick = #btnCheck")
 	public void onClickbtnCheck() {
-		
+
 	}
-
-
 
 	@Listen("onClick = #btnMain")
 	public void onClickbtnMain() {
@@ -57,41 +52,37 @@ public class Unit16 extends SelectorComposer<Component> {
 	@Listen("onClick = #btnReset")
 	public void onClickbtnReset() {
 		ArrayPattern();
-	
+
 	}
 
 	public void ArrayPattern() {
 
-		 int length = 12;   // độ dài mảng
-	        int K = 10;        // tổng 4 số liên tiếp
-	        int[] arr = new int[length];
-	        Random rand = new Random();
+		int length = 12; // độ dài mảng
+		int K = 10; // tổng 4 số liên tiếp
+		int[] arr = new int[length];
+		Random rand = new Random();
 
-	        // Sinh 3 số đầu ngẫu nhiên
-	        arr[0] = rand.nextInt(6); // ví dụ: giá trị 0..5
-	        arr[1] = rand.nextInt(6);
-	        arr[2] = rand.nextInt(6);
+		// Sinh 3 số đầu ngẫu nhiên
+		arr[0] = rand.nextInt(6); // ví dụ: giá trị 0..5
+		arr[1] = rand.nextInt(6);
+		arr[2] = rand.nextInt(6);
 
-	        // Tính số thứ 4 để đảm bảo tổng = K
-	        arr[3] = K - (arr[0] + arr[1] + arr[2]);
+		// Tính số thứ 4 để đảm bảo tổng = K
+		arr[3] = K - (arr[0] + arr[1] + arr[2]);
 
-	        // Sinh chu kỳ 4 cho các phần tử còn lại
-	        for (int i = 4; i < length; i++) {
-	            arr[i] = arr[i - 4];
-	        }
+		// Sinh chu kỳ 4 cho các phần tử còn lại
+		for (int i = 4; i < length; i++) {
+			arr[i] = arr[i - 4];
+		}
 
-	        txtSoHang0.setValue(String.valueOf(arr[0]));
-	        txtSoHang5.setValue(String.valueOf(arr[5]));
-	        txtSoHang10.setValue(String.valueOf(arr[10]));
-	        
-	        
-	        // In mảng
-	        System.out.print("Mảng: ");
-	        for (int x : arr) {
-	            System.out.print(x + " ");
-	        }
+		txtSoHang0.setValue(String.valueOf(arr[0]));
+		txtSoHang5.setValue(String.valueOf(arr[5]));
+		txtSoHang10.setValue(String.valueOf(arr[10]));
+
+		// In mảng
+		System.out.print("Mảng: ");
+		for (int x : arr) {
+			System.out.print(x + " ");
+		}
 	}
-
-	
-
 }
